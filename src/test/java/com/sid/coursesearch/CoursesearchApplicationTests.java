af4962bf9ee23ae8b580460ec4ddcb5c853af2aa
aapkakaptaan <siddhantcreate@gmail.com>
2025-08-03 17:13:37 +0530

    Initial commit

---
+++ src/test/java/com/sid/coursesearch/CoursesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.coursesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
